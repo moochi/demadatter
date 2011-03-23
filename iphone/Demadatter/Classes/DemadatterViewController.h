bdabc5c70e5d98010c9b6f2e0299f4ab8695cbcb
mochida <mochida@ups.ne.jp>
2011-03-23 11:15:36 +0900

    commit iphone client

---
+++ iphone/Demadatter/Classes/DemadatterViewController.h
@@ -0,0 +1,16 @@
+//
+//  DemadatterViewController.h
+//  Demadatter
+//
+//  Created by Yoshiaki NAKANISHI on 11/03/21.
+//  Copyright 2011 ブライテクノ株式会社. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DemadatterViewController : UIViewController {
+
+}
+
+@end
+
