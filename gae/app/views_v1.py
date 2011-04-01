546f29b3e1273b1a6c289ef68449d522f9a3ff10
mochida <mochida@ups.ne.jp>
2011-04-02 02:11:55 +0900

    v1 source commit

--- gae/app/views_v1.py
+++ gae/app/views_v1.py
@@ -16,31 +16,83 @@ import os, sys
 root_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(root_dir, 'lib'))
 
+import logging
+import random
 from flask import Module,  render_template
 from flask import make_response, Response
 from flask import request
 from flask import json
+from flask import redirect
 import logging
 from twitter import get_status_by_tweet_id
 import models
 views_v1 = Module(__name__)
 
+logging.getLogger().setLevel(logging.DEBUG)
+
 @views_v1.route('/', methods=['GET', 'POST'])
-def dema_ranking
-    flask.redirect('/v2/ranking')
+def dema_ranking():
+    return redirect('/v2/ranking')
 
 @views_v1.route('/count', methods=['GET', 'POST'])
-def dema_count
-    token	= request.args.get('token')
+def dema_count():
+    token	= getToken(request.args.get('token'))
     tweetid = request.args.get('tweetid')
     result = ['token=%s' % token]
     tweet = get_twit(tweetid)
-	result.append('%s=%d' % (tweetid,tweet.dema_count))
-	response = makeresponse(result)
-	response['Content-Type'] = "text/plain"
-	response['Content-Length'] = '%d' % len(result)
-	return response
+    if tweet:
+        result.append('%s=%d' % (tweetid,tweet.dema_count))
+    else:
+        result.append('%s=0' % tweetid)
+    result = '\n'.join(result)
+    response = make_response(result)
+    response.headers['Content-Type'] = "text/plain"
+    response.headers['Content-Length'] = '%d' % len(result)
+    return response
+
+@views_v1.route('/add', methods=['GET', 'POST'])
+def dema_add():
+    token   = getToken(request.args.get('token'))
+    result = ['token=%s' % token]
+
+    tweetid = request.args.get('tweetid')
+    # レポート追加
+    # models.Report(token=token,tweetid=tweetid).put()
+    # # ツイート更新
+    # tweet = models.Tweet.all().filter('tweetid =', tweetid).get() or models.Tweet()
+    # # ツイートID
+    # tweet.tweetid = tweetid
+    # # ツイート日時
+    # tweet.createdat = datetime.fromtimestamp(float(request.args.get('created_at')))
+    # # 本文
+    # tweet.tweet = request.args.get('tweet')
+    # # 発言者のスクリーンネーム
+    # tweet.screen_name = request.args.get('screen_name')
+    # # 発言者のユーザ名
+    # tweet.user_name = request.args.get('user_name')
+    # # レポート件数
+    # tweet.count = models.Report.all().filter('tweetid =', tweetid).count()
+    # # 保存
+    # tweet.put()
+    # result.append('%s=%d' % (tweetid, tweet.count))
+    result.append('%s=%d' % (tweetid, 1))
+
+    # 結果出力
+    result = '\n'.join(result)
+    response = make_response(result)
+    response.headers['Content-Type'] = "text/plain"
+    response.headers['Content-Length'] = '%d' % len(result)
+    return response
 
 def get_twit(tweet_id):
-    entity = models.Tweet.all().filter('tweet_id = ',tweet_id).get()
-	return entity
+    #logging.debug('hoge %s' % (tweet_id))
+    entity = models.Tweet.all().filter('tweet_id =',int(tweet_id)).get()
+    return entity
+
+def getToken(token):
+    if not token:
+        # 新しいトークンを発行
+        token = ''.join(random.choice(string.ascii_letters) for i in range(20))
+        models.User(token=token).put()
+    return token
+
